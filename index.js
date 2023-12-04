fc80ec12fc48228eb0ef5522ef3d5613e3e1f11e
bHabawel <rosshabawel12@gmail.com>
2023-12-05 01:55:22 +0800

    additional features: added styles & some elements

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let todoInput = document.getElementById("todoInput");
 let addTask = document.getElementById("addTask");
 let taskList = document.getElementById("taskList");
+let app = document.getElementById("app");
+
+let title = document.createElement("h1");
+title.innerHTML = "Todo List";
+app.prepend(title);
 
 addTask.addEventListener("click", () => {
   let listItem = document.createElement("li");
   let removeItem = document.createElement("button");
   let spanItem = document.createElement("span");
 
-  removeItem.innerHTML = "X";
+  removeItem.innerHTML = "Delete";
   spanItem.innerHTML = todoInput.value;
 
   if (spanItem.innerHTML === "") {
