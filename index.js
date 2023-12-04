ced812a8a7a6f8b4dbb56cec33711ebbee55a250
bHabawel <rosshabawel12@gmail.com>
2023-12-04 23:21:47 +0800

    handling adding new lists

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 let todoInput = document.getElementById("todoInput");
 let addTask = document.getElementById("addTask");
 let taskList = document.getElementById("taskList");
+
+addTask.addEventListener("click", () => {
+  let listItem = document.createElement("li");
+  let removeItem = document.createElement("button");
+  let spanItem = document.createElement("span");
+
+  removeItem.innerHTML = "X";
+  spanItem.innerHTML = todoInput.value;
+
+  listItem.appendChild(removeItem);
+  listItem.appendChild(spanItem);
+
+  taskList.appendChild(listItem);
+});
